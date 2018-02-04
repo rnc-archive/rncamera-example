b60840cd62f16bb2fa443a68266ec3d806eb0742
Jonas Eckerström <jonaseck@gmail.com>
2018-02-04 22:58:10 +0100

    simplified script

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 if [ ! -f ios/Frameworks/FaceDetector/Frameworks/frameworks/FaceDetector.framework/FaceDetector ]; then
     echo "Downloading and copying GMV..."
-    mkdir scripts/dirtemp
-    curl -o scripts/dirtemp/temp.tar.gz 'https://dl.google.com/dl/cpdc/df83c97cbca53eaf/GoogleMobileVision-1.1.0.tar.gz' &> /dev/null
-    tar -xvzf scripts/dirtemp/temp.tar.gz -C scripts/dirtemp &> /dev/null
-    cp -r scripts/dirtemp/FaceDetector/Frameworks ios/Frameworks/FaceDetector
-    rm -rf scripts/dirtemp
+    mkdir -p ios/Frameworks/FaceDetector
+    curl -sSL 'https://dl.google.com/dl/cpdc/df83c97cbca53eaf/GoogleMobileVision-1.1.0.tar.gz' | tar xz -C ios/Frameworks/FaceDetector --strip-components=2 FaceDetector
 fi
